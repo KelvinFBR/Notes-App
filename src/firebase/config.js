6e2ffc4a147d520ff732a707bb0ae59d826830c9
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-11-11 01:08:53 -0400

    [update] config firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore/lite";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyC0bygojRxxDwvizdN2ocXHh4pM6FdkJBs",
+  authDomain: "react-notes-app-f781e.firebaseapp.com",
+  projectId: "react-notes-app-f781e",
+  storageBucket: "react-notes-app-f781e.appspot.com",
+  messagingSenderId: "1049588980217",
+  appId: "1:1049588980217:web:22a405f1149a85e587c528",
 };
 
 export const FirebaseApp = initializeApp(firebaseConfig);
